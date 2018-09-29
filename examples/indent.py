25ebc10820e62d6df00a7799ae33dea5a0f80e58
Ashish <ashishinnovate@gmail.com>
2018-09-29 22:21:46 +0530

    indentation

---
+++ examples/indent.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+#following two lines are statements
+print("hello world")
+TestVar=""
+
+#below is an if and else block
+if TestVar:
+    print TestVar
+else:
+    print("TestVar is null")
