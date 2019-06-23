29d01e355fd48f4b337f5659c7cf04173ff85d2d
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2019-06-23 22:55:11 +0530

    Create whoops.py

---
+++ examples/whoops.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+'''
+for i in range(1,10):
+	print i
+
+my_list=[112,43,12,"sdcdsc","9nu"]
+
+for i in my_list:
+	print i
+
+my_tuple=(112,43,12,"sdcdsc","9nu")
+for i in my_tuple:
+	print i
+'''
