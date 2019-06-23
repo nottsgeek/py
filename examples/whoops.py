6877d111373445cc4be4139d2bbddd0a7ab0d574
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2019-06-23 23:21:13 +0530

    Update whoops.py

--- examples/whoops.py
+++ examples/whoops.py
@@ -11,4 +11,19 @@ for i in my_list:
 my_tuple=(112,43,12,"sdcdsc","9nu")
 for i in my_tuple:
 	print i
+
+my_tuple=(112,43,12,"sdcdsc","9nu")
+for i in range(0, len(my_tuple)):
+	print(my_tuple[i])
+
+#Read a list and create a tupple with if condition satisfies
+
+my_list1=[]
+my_tuple=[112,"test"]
+my_list=[112,43,12,"sdcdsc","9nu"]
+for i in my_list:
+	if i in my_tuple:
+		my_list1.append(i)
+
+print my_list1
 '''
