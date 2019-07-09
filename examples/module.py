c109ebe72d8c31ccd8dabb7afe428693716f759e
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2019-07-10 01:23:57 +0530

    Create module.py

---
+++ examples/module.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import random
+import math
+import datetime 
+try
+except
+
+
+#print(os.getcwd())
+os.mkdir('tempDir')
+
+if not os.path.exists(dirName):
+    os.mkdir(dirName)
+
+#print math.sqrt(25) 
+#x = datetime.datetime.now()
+#print(x)
+
+print random.randint(0, 5)
+
+myList = ["aws", "connector", 37, 5, "port", 473, "wwn"]
+random.choice(myList)
