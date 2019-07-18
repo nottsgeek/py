f9cbab0573a3d75865e7bc3346d045251979e664
nottsgeek <42448638+nottsgeek@users.noreply.github.com>
2019-07-18 09:48:18 +0530

    comments

--- examples/start_instances.py
+++ examples/start_instances.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python
 
+#Script to start stopped instances in an aws account
+
 import json
 import os
 
-os.system("aws ec2 list-instances --region=eu-west-2 >> instance_details")
+#get list of instances and store it in instance_details.json file
+os.system("aws ec2 list-instances --region=eu-west-2 >> instance_details.json")
 
-with open('instance_details') as fh:
+#Read the instance_details.json in script and convert data to dict
+with open('instance_details.json') as fh:
   data = json.load(fh)
 
-
+#determince the total instance count
 instance_count = len(data["Reservations"])
+
+#Looping through all instances, store the id and state in vars
 for i in range(0,instance_count):
   InstanceId=data["Reservations"][i]["Instances"][0]["InstanceId"]
   InstanceState=data["Reservations"][i]["Instances"][0]["State"]["Name"]
+  #if state is stopped perform a start on id
   if InstanceState == "stopped":
     print("%s is stopped" %InstanceId)
     os.system("aws ec2 start-instances --region=eu-west-2 --instance-ids %s" %InstanceId)
